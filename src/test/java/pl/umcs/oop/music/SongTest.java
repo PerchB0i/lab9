04645f183038a36cd7e3aea67db599987762522d
KaminaPL <malytankus@gmail.com>
2024-05-09 15:08:07 +0200

    Parameterized tests

--- src/test/java/pl/umcs/oop/music/SongTest.java
+++ src/test/java/pl/umcs/oop/music/SongTest.java
@@ -3,10 +3,14 @@ package pl.umcs.oop.music;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pl.umcs.oop.database.DatabaseConnection;
 
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +18,22 @@ class SongTest {
     @BeforeAll
     public static void setup() {
         DatabaseConnection.connect("songs.db");
+    }
+
+    private static Stream<Arguments> songs() {
+        return Stream.of(
+                Arguments.arguments(1,"The Beatles","Hey Jude",431),
+                Arguments.arguments(2,"The Rolling Stones","(I Can't Get No) Satisfaction",224),
+                Arguments.arguments(3,"Led Zeppelin","Stairway to Heaven",482)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("songs")
+    public void streamTest(int index, String artist, String title, int length) throws SQLException {
+        Optional<Song> song;
+        song = Song.Persistence.read(index);
+        assertEquals(title, song.get().title());
     }
 
     @Test
